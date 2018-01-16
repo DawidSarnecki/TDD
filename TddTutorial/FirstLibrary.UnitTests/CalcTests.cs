245873fc05e55206b20376eb739891bcb0e49740
DawidSarnecki <test@solwit.com>
2018-01-16 20:50:51 +0100

    Test method using DataRow attributes.

--- TddTutorial/FirstLibrary.UnitTests/CalcTests.cs
+++ TddTutorial/FirstLibrary.UnitTests/CalcTests.cs
@@ -5,7 +5,7 @@ namespace FirstLibrary.UnitTests
     //ref: https://dariuszwozniak.net/2013/06/30/kurs-tdd-czesc-4-nasz-pierwszy-test-jednostkowy/
 
     [TestClass]
-    public class Calc_Tests
+    public class CalcTests
     {
         [TestMethod]
         public void AddsTwoNumbers_Calculated()
@@ -20,6 +20,16 @@ namespace FirstLibrary.UnitTests
             // the same shortly
             Assert.AreEqual(-1, calc.Add(5, -6));
             Assert.AreEqual(0, calc.Add(-20, 20));
+        }
+
+        [DataTestMethod]
+        [DataRow(5,- 6, -1)]
+        [DataRow(-20, 20, 5)]
+        [DataRow(0, 0, 1)]
+        public void AddsTwoNumbers_CalculatedUsingDataRow(int a, int b, int result)
+        {
+            var calc = new Calc();
+            Assert.AreEqual(result, calc.Add(a, b));
         }
     }
 }
