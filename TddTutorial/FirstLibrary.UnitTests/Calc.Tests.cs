239c9e0643605d91a31bdf68c37a21b0e0440528
DawidSarnecki <test@solwit.com>
2018-01-16 20:26:37 +0100

    Initial implementation of UT.

---
+++ TddTutorial/FirstLibrary.UnitTests/Calc.Tests.cs
@@ -0,0 +1,25 @@
+﻿
+namespace FirstLibrary.UnitTests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    //ref: https://dariuszwozniak.net/2013/06/30/kurs-tdd-czesc-4-nasz-pierwszy-test-jednostkowy/
+
+    [TestClass]
+    public class Calc_Tests
+    {
+        [TestMethod]
+        public void AddsTwoNumbers_Calculated()
+        {
+            // arange
+            var calc = new Calc();
+            // act
+            int result = calc.Add(2, 2);
+            // assert
+            Assert.AreEqual(4, result);
+
+            // the same shortly
+            Assert.AreEqual(-1, calc.Add(5, -6));
+            Assert.AreEqual(0, calc.Add(-20, 20));
+        }
+    }
+}
